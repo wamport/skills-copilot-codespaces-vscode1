26d55d428caba840e6e08aceafb780110e291427
wamport <163754205+wamport@users.noreply.github.com>
2024-03-17 18:26:51 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,10 +1,47 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
-});
+//create web server
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var querystring = require('querystring');
+var comments = [];
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
-});
+http.createServer(function(req, res) {
+    //parse the url
+    var urlObj = url.parse(req.url, true);
+    if (urlObj.pathname === '/') {
+        fs.readFile('index.html', function(err, data) {
+            if (err) {
+                res.writeHead(500, {
+                    'Content-Type': 'text/plain'
+                });
+                res.end(err);
+            }
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            res.end(data);
+        });
+    } else if (urlObj.pathname === '/comment') {
+        //get the data
+        var data = urlObj.query;
+        comments.push(data);
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.end(JSON.stringify(comments));
+    } else {
+        fs.readFile(urlObj.pathname.substring(1), function(err, data) {
+            if (err) {
+                res.writeHead(404, {
+                    'Content-Type': 'text/plain'
+                });
+                res.end('Page not found');
+            }
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            res.end(data);
+        });
+    }
+}).listen(3000);
+console.log('Server running at http://localhost:3000/');
